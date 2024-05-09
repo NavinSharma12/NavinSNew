7289aad3d1ed22f57df9a3c95955a65682d7d094
NavinSharma12 <navinguddu@hotmail.com>
2024-05-09 12:59:48 +0530

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,6 +80,7 @@
     })
   }
 
+  
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
@@ -271,6 +272,50 @@
     }
   });
 
+  $(function(){
+  
+    var swiper = new Swiper('.carousel-gallery .swiper-container', {
+      effect: 'slide',
+      speed: 900,
+      slidesPerView: 5,
+      spaceBetween: 20,
+      simulateTouch: true,
+      autoplay: {
+        delay: 5000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: '.carousel-gallery .swiper-pagination',
+        clickable: true
+      },
+      breakpoints: {
+        // when window width is <= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 5
+        },
+        // when window width is <= 600px
+        600: {
+          slidesPerView: 2,
+          spaceBetween: 10
+        },
+        // when window width is <= 800px
+        900: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is <= 800px
+        2800: {
+          slidesPerView: 5,
+          spaceBetween: 20
+        }
+      }
+    }); /*http://idangero.us/swiper/api/*/
+  
+    
+  
+  });
   /**
    * Animation on scroll
    */
